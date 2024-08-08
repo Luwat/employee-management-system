376b36d2ff68e0ad4f504c98b74e700be0bf9cfc
luwat <oluwatobioreniyi@gmail.com>
2024-08-08 11:26:35 +0100

    Add dashboard page and sidenav component

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+import SideNav from '@/components/side-nav'
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <SideNav />
+  )
+}
+
+export default Layout
---
+++ components/side-nav.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const SideNav = () => {
+  return <section className="h-screen w-1/5 bg-neutral-700 text-neutral-100 p-8 fixed">
+    <div >
+        <Link href={'/dashboard'}>
+            <h1>Home</h1>
+        </Link>
+        <Link href={'/dashboard/employees'}>
+            <h1>Employees</h1>
+        </Link>
+        <Link href={`/dashboard/${'id'}`}>
+            <h1>Employee Details</h1>
+        </Link>
+    </div>
+  </section>;
+};
+
+export default SideNav;
