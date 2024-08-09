246a57d28c30d66d069e86a2404765d246892200
luwat <oluwatobioreniyi@gmail.com>
2024-08-09 23:27:06 +0100

    Remove employee details link

--- components/side-nav.tsx
+++ components/side-nav.tsx
@@ -1,6 +1,7 @@
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import Link from "next/link";
 
-const SideNav = () => {
+const SideNav = ({ id }: { id: string }) => {
   return (
     <section className="h-screen w-1/5 bg-neutral-700 text-neutral-100 p-8 bg-fixed">
       <nav>
@@ -13,13 +14,6 @@ const SideNav = () => {
           <li>
             <Link href={"/employees"}>
               <h1>Employees</h1>
-            </Link>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <Link href={`/employees/${"id"}`}>
-              <h1>Employee Details</h1>
             </Link>
           </li>
         </ul>
