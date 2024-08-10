d9a28e2fb0dab27856bf25494525791e7af09c5b
luwat <oluwatobioreniyi@gmail.com>
2024-08-10 22:41:47 +0100

    Add employees Link button to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome home man.</h1>
+      <Link href={'/employees'}><button className="bg-neutral-700 text-neutral-100 py-2 px-4 rounded">Employees Page</button></Link>
     </main>
   );
 }
