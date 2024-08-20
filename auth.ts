f7c2a568ca22c44cf31b709c73b9ee1cf0c32bf2
luwat <oluwatobioreniyi@gmail.com>
2024-08-20 20:32:28 +0100

    Restart authentication logic

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth";
-import Auth0 from "next-auth/providers/auth0";
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Auth0],
-})
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+});
