2cfb2c8d9a75291169d68ec3b727fa417f240992
luwat <oluwatobioreniyi@gmail.com>
2024-08-21 12:46:30 +0100

    Authenticate user logout

--- components/side-nav.tsx
+++ components/side-nav.tsx
@@ -1,3 +1,4 @@
+import { signOut } from "@/auth";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import Link from "next/link";
 
@@ -8,6 +9,16 @@ const SideNav = ({ id }: { id: string }) => {
         <ul>
           <li>
             <Link href={"/"}>Home</Link>
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-xl font-medium hover:bg-sky-100 text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                <div className="">Sign Out</div>
+              </button>
+            </form>
           </li>
         </ul>
         <ul>
