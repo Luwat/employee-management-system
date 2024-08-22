2a72f30d10201d2e272dd6bed75a527c95e7630d
luwat <oluwatobioreniyi@gmail.com>
2024-08-22 21:52:56 +0100

    Style login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,11 +5,7 @@ export default function LoginPage() {
   return (
     <>
       <Header />
-      <main className="flex items-center justify-center h-screen">
-        <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-          <SignIn />
-        </div>
-      </main>
+      <SignIn />
     </>
   );
 }
--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -1,73 +1,81 @@
-'use client'
+"use client";
 
 import { authenticate } from "@/lib/action";
-import { useActionState } from "react";
+import { Butterfly_Kids } from "next/font/google";
 import { useFormState } from "react-dom";
+import Button from "./Button";
 
 export default function SignIn() {
   const [errorMessage, formAction, isPending] = useFormState(
     authenticate,
-    undefined,
+    undefined
   );
   return (
-    <form action={formAction} className="space-y-3">
-      <div
-        className="flex-1 rounded-
-      lg bg-gray-50 px-6 pb-4 pt-8"
+    <section className="flex items-center justify-center h-[88vh]">
+      <form
+        action={formAction}
+        className="w-1/4  bg-neutral-100 shadow-2xl shadow-neutral-700 rounded-2xl py-8 flex justify-center items-center"
+        autoComplete="off"
       >
-        <h1 className={` mb-3 text-2xl`}>Please log in to continue.</h1>
-        <div className="w-full">
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-              />
+        <div className="">
+          <h1 className={` mb-3 text-2xl`}>Please log in to continue.</h1>
+          <div className="w-full">
+            <div>
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email address"
+                  required
+                />
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                  required
+                  minLength={6}
+                />
+              </div>
             </div>
           </div>
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
-              />
-            </div>
+          <Button
+            type="submit"
+            className="bg-neutral-700 text-neutral-100 hover:text-neutral-300 mt-5"
+            aria-disabled={isPending}
+          >
+            Login
+          </Button>
+          <div
+            className="flex h-2 items-end space-x-1"
+            aria-live="polite"
+            aria-atomic="true"
+          >
+            {errorMessage && (
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            )}
           </div>
         </div>
-        <button type="submit" aria-disabled={isPending}>
-          Login
-        </button>
-        <div
-          className="flex h-8 items-end space-x-1"
-          aria-live="polite"
-          aria-atomic="true"
-        >
-          {errorMessage && (
-            <p className="text-sm text-red-500">{errorMessage}</p>
-          )}
-        </div>
-      </div>
-    </form>
+      </form>
+    </section>
   );
 }
